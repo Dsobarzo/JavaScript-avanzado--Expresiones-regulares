e9af4bdbcd81153b933f0fe17891dd2dc9b94529
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-31 17:54:43 -0300

    nueva expresion
    Se agrego nueva expresion regular expresion2

--- 01_Conceptos_basicos/04_Uso_de_atajos_de_grupos_de_caracteres/app.js
+++ 01_Conceptos_basicos/04_Uso_de_atajos_de_grupos_de_caracteres/app.js
@@ -1,6 +1,8 @@
 //var expresion1 = /[0123456789]/;
 var expresion1 = /\d\d-\d\d-\d\d\d\d/;
+var expresion2 = /\d\d-\w\w\w\w\w-\d\d\d\d/;
 
 console.log(expresion1.test('29-junio-2007'));
+console.log(expresion2.test('29-junio-2007'));
 
 //29-06-2007
