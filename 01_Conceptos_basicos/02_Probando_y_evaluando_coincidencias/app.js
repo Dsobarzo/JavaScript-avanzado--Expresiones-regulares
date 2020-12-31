27ecf88cac4a493c058a7a6aaa2b64d0b5d1a1b3
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-31 17:41:04 -0300

    New console.log
    Se agrego nuevo consol.log para verificar exprecion regular1

--- 01_Conceptos_basicos/02_Probando_y_evaluando_coincidencias/app.js
+++ 01_Conceptos_basicos/02_Probando_y_evaluando_coincidencias/app.js
@@ -4,3 +4,5 @@ console.log(expresion1.test('abcdef'));
 var expresion2 = /Hola Mundo/;
 
 console.log(expresion2.test('El primer programa es Hola Mundo'));
+
+console.log2 (expresion1.test( '123'));
